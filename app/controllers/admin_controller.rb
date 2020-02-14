e0a38fb7189d2ce66fe10ec9453fd9389a1b2c4d
Vatsal Mewada <vatsal.mewada@bacancytechnology.com>
2020-02-14 18:22:31 +0530

    panel functionality added

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,22 +1,37 @@
 class AdminController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+  
   def new
+    @user = User.new
   end
 
   def create
-  end
-
-  def update
+    @user = User.new(info_params)
+    if @user.save
+      redirect_to admin_index_path
+    end
   end
 
   def edit
   end
 
+  def update
+  end
+
   def destroy
   end
 
-  def show
+  def addfaculty
   end
 
-  def index
+  def info_params
+    params.required(:info).permit(:name, :password, :address)
   end
+
+
+
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,26 @@
 class ApplicationController < ActionController::Base
 
+    protect_from_forgery with: :exception
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
     def after_sign_in_path_for(resource)
         if(current_user.has_role?(:faculty))
             faculty_index_path
         elsif(current_user.has_role?(:admin))
             admin_index_path
-        else
+        elsif(current_user.has_role?(:student))
             student_index_path
+        else
+            
         end
       end
+
+      protected
+
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:enrollment ,:sem ,:fname, :lname, :email, :branch, :password, :mobile, :pyear, :status) }
+            devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:enrollment ,:sem ,:fname, :lname, :email, :branch, :password, :mobile, :pyear, :status) }
+        end 
 
 end
