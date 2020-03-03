ca7b883367b16fb06e9868e7aa813b2682476df5
vatsalmewada <vatsal.mewada@bacancytechnology.com>
2020-03-03 17:34:55 +0530

    subjects controller changed

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -2,55 +2,53 @@
 
 # add subject
 class SubjectsController < ApplicationController
+  before_action :set_student, only: %i[edit update destroy]
   def index
     @subjects = Subject.all
   end
 
   def new
-    #  @user = Usaer.new
     @subject = Subject.new
-    @subjects = Subject.all
   end
 
   def create
-    puts '   ------------------hii'
     # byebug
     @subject = Subject.new(info_params)
     @subject.branch_id = params[:branch_id]
     @subject.semester_id = params[:semester_id]
     if @subject.save
-      flash[:notice] = 'Subject added Successfully'
-      redirect_to subjects_path
+      redirect_to subjects_path, notice: 'Subject added Successfully'
     else
       flash[:alert] = 'Something went wrong!'
-
+      render 'new'
     end
   end
 
-  def edit
-    @subject = Subject.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @subject = Subject.find(params[:id])
     @subject.branch_id = params[:branch_id]
     @subject.semester_id = params[:semester_id]
     if @subject.update(info_params)
       redirect_to subjects_path, notice: 'Subject updated Successfully'
     else
-      byebug
-      render 'edit'
       flash[:alert] = 'Something went wrong!'
+      render 'edit'
     end
   end
 
   def destroy
-    @subject = Subject.find(params[:id])
-    @subject.destroy!
+    @subject.destroy
     redirect_to subjects_path
   end
 
+  private
+
   def info_params
     params.required(:subject).permit(:branch_id, :semester_id, :name)
+  end
+
+  def set_subjects
+    @subject = Subject.find(params[:id])
   end
 end
